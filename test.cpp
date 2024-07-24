77d0c37c3951546f675c26be797a02092efbc0e0
包子 <2994963878@qq.com>
2024-07-24 21:49:42 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -6,19 +6,6 @@ int main() {
 
 
 	cout << "Hello World!\n" << endl;
-	cout << "我是傻逼，哈哈哈哈哈" << endl;
-	cout << "我是大傻瓜，哈哈哈哈哈" << endl;
-	cout << "432432432432432432432432432" << endl;
-	cout << "楼上是傻瓜" << endl;
-	cout << "楼上是傻瓜" << endl;
-	cout << "楼上是傻瓜" << endl;
-	cout << "楼上是傻瓜" << endl;
-	cout << "楼上是傻瓜" << endl;
-	cout << "楼上是傻瓜" << endl;
-	cout << "楼上是傻瓜" << endl;
-	cout << "楼上是傻瓜" << endl;
-	cout << "楼上是傻瓜" << endl;
-	cout << "楼上是傻瓜" << endl;
-	cout << "OK，兄弟们，全体目光向我看齐，看我看我，我宣布个事" << endl;
+	
 	return 0;
 }
