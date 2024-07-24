7ae4d2d778748256e33bdc331124581c4410cf60
Chi-Nian <142979464+Chi-Nian@users.noreply.github.com>
2024-07-24 22:13:27 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -6,6 +6,7 @@ int main() {
 
 
 	cout << "Hello World!\n" << endl;
+<<<<<<< HEAD
 	cout << "我是傻逼" << endl;
 	cout << "woshidashabi" << endl;
 	cout << "我是傻逼" << endl;
@@ -18,5 +19,14 @@ int main() {
 	cout << "woshidashabi" << endl;
 	cout << "我是傻逼" << endl;
 	cout << "woshidashabi" << endl;
+=======
+	cout << "Hello World!\n" << endl;
+	cout << "Hello World!\n" << endl;
+	cout << "Hello World!\n" << endl;
+	cout << "Hello World!\n" << endl;
+	cout << "Hello World!\n" << endl;
+	cout << "Hello World!\n" << endl;
+	
+>>>>>>> 587c75008562ef064668f6c45dd9f6c306fb3bff
 	return 0;
 }
