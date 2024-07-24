76dfc2803e0fe2f2f02bdb3ef600ba0517a57df3
包子 <2994963878@qq.com>
2024-07-24 22:28:56 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -6,17 +6,6 @@ int main() {
 
 
 	cout << "Hello World!\n" << endl;
-	cout << "我是傻逼" << endl;
-	cout << "woshidashabi" << endl;
-	cout << "我是傻逼" << endl;
-	cout << "woshidashabi" << endl;
-	cout << "我是傻逼" << endl;
-	cout << "woshidashabi" << endl;
-	cout << "我是傻逼" << endl;
-	cout << "woshidashabi" << endl;
-	cout << "我是傻逼" << endl;
-	cout << "woshidashabi" << endl;
-	cout << "我是傻逼" << endl;
-	cout << "woshidashabi" << endl;
+	
 	return 0;
 }
