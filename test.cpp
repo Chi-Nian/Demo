9df984068f8f94972f915df874c6c7c17cf34953
Chi-Nian <142979464+Chi-Nian@users.noreply.github.com>
2024-07-26 17:54:36 +0800

    666

--- test.cpp
+++ test.cpp
@@ -7,20 +7,6 @@ int main() {
 
 	cout << "厘頁艦苧繁~" << endl;
 	cout << "込込込込込込込" << endl;
-	cout << "込込込込込込込" << endl;
-	cout << "込込込込込込込" << endl;
-	cout << "込込込込込込込" << endl;
-	cout << "込込込込込込込" << endl;
-	cout << "込込込込込込込" << endl;
-	cout << "込込込込込込込" << endl;
-	cout << "込込込込込込込" << endl;
-	cout << "込込込込込込込" << endl;
-	cout << "込込込込込込込" << endl;
-
-	cout << "11111" << endl;
-	cout << "11111" << endl;
-	cout << "11111" << endl;
-	cout << "11111" << endl;
 	
 	return 0;
 }
