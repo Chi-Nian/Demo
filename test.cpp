17850de714017e02e93bb0ff1ef411590bbe636f
包子 <2994963878@qq.com>
2024-07-26 19:40:15 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -5,15 +5,8 @@ using namespace std;
 int main() {
 
 
-<<<<<<< HEAD
-	cout << "Hello World!\n" << endl;
-=======
+
 	cout << "ÎÒÊÇ´ÏÃ÷ÈË~" << endl;
-	cout << "¹þ¹þ¹þ¹þ¹þ¹þ¹þ" << endl;
->>>>>>> 766927e339e097390d3936e0db7960ca37de8ef5
-	
-	cout << "1" << endl;
-	cout << "1" << endl;
-	cout << "1" << endl;
+
 	return 0;
 }
