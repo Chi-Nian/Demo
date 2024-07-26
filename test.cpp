ffbbf058b58e016b07e6fc38f957698f55aecb81
包子 <2994963878@qq.com>
2024-07-26 20:44:33 +0800

    怎么回事

--- test.cpp
+++ test.cpp
@@ -6,5 +6,9 @@ int main() {
 
 
 	cout << "Hello World!\n" << endl;
+	cout << "怎么回事小老弟\n" << endl;
+	cout << "怎么回事小老弟\n" << endl;
+	cout << "怎么回事小老弟\n" << endl;
+	cout << "怎么回事小老弟\n" << endl;
 	return 0;
 }
