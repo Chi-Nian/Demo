8740198fe2de218b49c23f33e7d2402ecf36b8fe
Chi-Nian <142979464+Chi-Nian@users.noreply.github.com>
2024-07-26 18:42:37 +0800

    66

--- test.cpp
+++ test.cpp
@@ -6,6 +6,11 @@ int main() {
 
 
 	cout << "我是聪明人~" << endl;
+	cout << "我是聪明人~" << endl;
+	cout << "我是聪明人~" << endl;
+	cout << "我是聪明人~" << endl;
+	cout << "我是聪明人~" << endl;
+	cout << "我是聪明人~" << endl;
 	
 	return 0;
 }
